e0c12eb0c3ed7ea928445920d3e458c9373574aa
juan <jgironherranz@gmail.com>
2024-01-29 13:51:13 +0100

    textures working

--- src/viewer.cpp
+++ src/viewer.cpp
@@ -46,26 +46,27 @@ void Viewer::run()
     {
 
         float positions[] = {
-            -0.5f,
-            -0.5f,
-            0.5f,
-            -0.5f,
-            0.5f,
-            0.5f,
-            -0.5f,
-            0.5f,
+            -0.5f,-0.5f, 0.0f, 0.0f,
+             0.5f,-0.5f, 1.0f, 0.0f,
+             0.5f, 0.5f, 1.0f, 1.0f,
+            -0.5f, 0.5f, 0.0f, 1.0f
         };
 
         unsigned int indices[] = {
             0, 1, 2,
             2, 3, 0};
 
+        GLCall( glEnable(GL_BLEND) );
+        GLCall( glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA) );
+
         VertexArray va;
-        VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 
         VertexBufferLayout layout;
         layout.PushFloat(2);
+        layout.PushFloat(2);
         va.AddBuffer(vb, layout);
+
         IndexBuffer ib(indices, 6);
 
         Shader shader("res/shaders/Basic.shader");
@@ -92,9 +93,6 @@ void Viewer::run()
         while (!glfwWindowShouldClose(window))
         {
             renderer.Clear();
-
-            shader.Bind();
-            shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
 
             renderer.Draw(va, ib, shader);
 
